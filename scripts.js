96bb45e05be6cdb1fd7a60f5ac267a3ce3c37d26
Gaurav Kumar <gauravkr7103@gmail.com>
2025-06-12 01:46:50 +0530

    complete the Structure of Game Logic in js
    do look at you code. How you had used all the logic
    of game in Board and the rest are just its
    Games'function for implementation

--- scripts.js
+++ scripts.js
@@ -1,91 +1,132 @@
-const Board = (() => {
+const Board = (()=>{
     const rowSize = 3;
     const colSize = 3;
-    let board = [];//const can be used (because determines the object storage type)
     let moves = 0;
-    function resetBoard() {
+    let row = -1, col = -1;
+
+    let board = [];
+    function makeBoard() {
+        board = [];
+        moves = 0;
         for (let i=0; i<rowSize; i++) {
             board[i] = [];
             for (let j=0; j<colSize; j++) {
-                board[i].push(0);//0 means empty cell
+                board[i].push(0);
             }
         }
-        return board;
-    };
-    function askToMove () {
-        const row = Number(prompt("Enter the Row :"));
-        const col = Number(prompt("Enter the Col :"));
-        return [row,col];
-    };
-    function isSpaceAvailable(row,col) {
-        return (board[row][col] === 0);
-    }; 
-    function makeMove(token) {
-        let [i,j] = askToMove();
-        while (!isSpaceAvailable(i,j)) {
-            ([i,j] = askToMove());
+    }
+
+    function askLocation () {
+        row = Number(prompt("Enter the Row"));
+        col = Number(prompt("Enter the Column"));
+    }
+    function playerMove(token) {
+        askLocation();
+        if (board[row][col] !==0) {
+            playerMove(token);
+            return;
         }
-        board[i][j] = token;
-        movesMade();
-        return hasPlayerWon(i,j);
-    };
-    function hasPlayerWon (row, col) {
-        const val = board[row][col];
-        for (let i=0; i<rowSize; i++) 
-            if (board[i][col] !== val)
-                return false;
-        for (let j=0; j<colSize; j++)
-            if (board[row][j] !== val) 
-                return false;
-        return true;
-    };
-    function movesMade () {
-        moves++;
+        board[row][col] = token;
+        moves ++;
+    }
+    function movesCheck() {
         if (moves == 9) {
             console.log("Draw");
-            resetBoard();
+            makeBoard();
         }
     }
-    return {resetBoard, makeMove};
+    function hasPlayerWon() {
+        let val = row + col;
+        if (val % 2 == 0) {//check diagnols
+            //left diagnol
+            let i=0, j=0;
+            let ele = 0;
+            while (i<3 && j<3) {
+                if (board[i][j] === board[row][col]) {
+                    ele++;
+                    i++; j++;
+                } else {
+                    break;
+                }
+            }
+            if (ele == 3)
+                return true;
+
+            //right diagnol
+            i=0; j=2;
+            ele = 0;
+            while (i<3 && j>=0) {
+                if (board[i][j] === board[row][col]) {
+                    ele++;
+                    i++; j--;
+                } else {
+                    break;
+                }
+            }
+            if (ele == 3)
+                return true;
+        }
+        //now just have to check vertical rows and cols;
+        let ele = 0;
+        for (let i=0; i<rowSize; i++) {
+            if (board[i][col] === board[row][col])
+                ele++;
+            else break;
+        }
+        if (ele == 3)
+            return true;
+
+        ele = 0;
+        for (let j=0; j<colSize; j++) {
+            if (board[row][j] === board[row][col])
+                ele++;
+            else break;
+        }
+        if (ele == 3)
+            return true;
+
+        return false;
+    }
+    return {makeBoard, playerMove, hasPlayerWon, movesCheck};
 })();
 
-Board.resetBoard();
+function Player(name = "", token) {
+    const PlayerName = name;
+    const PlayerToken = token;
 
-function Player (name="", token) {
-    const playerName = name;
-    const playerToken = token;
-    function viewPlayerName() {
-        return playerName;
+    function sayPlayerName () {
+        return PlayerName;
     }
-    function viewPlayerToken() {
-        return playerToken;
+    function sayPlayerToken() {
+        return PlayerToken;
     }
-    function playerMove() {
-        Board.makeMove(playerToken);
-    }
+    return {sayPlayerName, sayPlayerToken}
+}
 
-    return {viewPlayerName, viewPlayerToken, playerMove};
-};
-
-const player1 = Player("player1", 'O');
-const player2 = Player("player2", 'X');
-
+Board.makeBoard();
+const player1 = Player("Player1", 'O');
+const player2 = Player("Player2", "X");
 const Game = ( () => {
-    function playGame() {
+    function Begin() {
         while (1) {
-            let won = player1.playerMove();
-            if (won) {
-                console.log(`${player1.viewPlayerName} has won`);
+            Board.playerMove(player1.sayPlayerToken());
+            if (Board.hasPlayerWon()) {
+                console.log(`${player1.sayPlayerName()} has won the game.`);
+                break;
+            } else {
+                Board.movesCheck();
             }
-            won = player2.playerMove();
-            if (won) {
-                console.log(`${player1.viewPlayerName} has won`);
+
+            Board.playerMove(player2.sayPlayerToken());
+            if (Board.hasPlayerWon()) {
+                console.log(`${player2.sayPlayerName()} has won the game.`);
+                break;
+            } else{
+                Board.movesCheck();
             }
-            won  
         }
     }
-    return {playGame};
-}
-)();
+    return {Begin};
+})();
 
-Game.playGame();
+Game.Begin();
